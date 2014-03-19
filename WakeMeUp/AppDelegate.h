64e70e62251442872108dad227c7bb596494297b
GenralDesigns <mgrg1999@gmail.com>
2014-03-18 18:02:26 -0700

    Initial Commit

---
+++ WakeMeUp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WakeMeUp
+//
+//  Created by Milo Gosnell on 3/18/14.
+//  Copyright (c) 2014 Milo Gosnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WakeMeUp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WakeMeUp
+//
+//  Created by Milo Gosnell on 3/18/14.
+//  Copyright (c) 2014 Milo Gosnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
